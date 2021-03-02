70c452a8860b7802883ddbe554a10a9f75b576b7
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-03-01 20:48:49 -0600

    fixed paths to go to correct html

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -12,9 +12,9 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
 
 // Starts the server to begin listening
 
